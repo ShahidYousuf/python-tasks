a65ba7c94ebf36f462cfe8b3dc40b6d43ec2ee6a
shahidyousuf <shahidyousuf77@gmail.com>
2018-08-11 15:43:30 +0530

    add directory for informal intro to python, and basic numbers file

---
+++ python-lang/informal-intro/numbers311.py
@@ -0,0 +1,20 @@
+# Numbers Basic
+print(2+2)
+print(50-5*6)
+print((50-5*6)/4)
+print(8/5) # floating point division
+
+print(17/3) # classic division, returns a float
+print(17//3) # floor division, discards fractional part
+print(17%3) # returns remainder
+
+print(5**2) # calculate power, 5 squared
+
+width = 20
+height = 5*9
+print(width*height)
+print(3+5j) # complex number
+c1 = 3 + 4j
+c2 = 3 - 4j
+print(c1 * c2) # complex math
+
