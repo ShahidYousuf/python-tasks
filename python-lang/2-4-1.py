3813eceb11b4f28616145c322e0624b67366eabf
shahidyousuf <shahidyousuf77@gmail.com>
2018-08-11 15:19:01 +0530

    Some escapes in python

---
+++ python-lang/2-4-1.py
@@ -0,0 +1,6 @@
+print("Hello Python")
+print("Hello Python\n")
+print("Hello Python\\and backslash")
+print("Hello Python Bell \a")
+print("Hello \t Python Tab")
+print("Hello \v Python Vertical Tab")
