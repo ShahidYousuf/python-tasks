5be44faf9ae4c63512847889cbecf3b09a2f8886
shahidyousuf <shahidyousuf77@gmail.com>
2018-08-13 11:57:19 +0530

    add more to functions

--- python-lang/control-flow/functions46.py
+++ python-lang/control-flow/functions46.py
@@ -3,12 +3,45 @@
 # Fibonacci sequence upto n
 
 def fibo(n):
+    """ Print a Fibonacci series upto a number n """
     a, b = 0, 1
     while a < n:
         print(a, end= ' ')
         a, b = b, a + b
     print()
 
+def myfunc(param, *args, **kwargs):
+    print("Ist param is %s" %param)
+    for item in args:
+        print(item)
+    for key in kwargs:
+        print(key, ':', kwargs[key])
+
 if __name__ == '__main__':
     fibo(50)
+    mylist = ['a', "something", 23.5, 23]
+    mydict = {'a':"apple", "b":"bat"}
+    myfunc("Hello there", mylist, mydict)#what if we pass an explicitlist/dict
+    print()
+    myfunc("New Hello", "a", "list", "of", "items", name="name", age=34) #normal
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
