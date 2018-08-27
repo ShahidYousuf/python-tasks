30654b95e846f902ef43553b9e23e53dbd23104a
shahidyousuf <shahidyousuf77@gmail.com>
2018-08-27 17:14:30 +0530

    add super and more inhertance

--- python-lang/class-basics/Class2.py
+++ python-lang/class-basics/Class2.py
@@ -12,20 +12,25 @@ class Person:
         return self.name + " " + str(self.job) + " " + str(self.pay)
 
 class Manager(Person):
-    def __init__(self, name, pay):
-        Person.__init__(self, name, 'Manager', pay)
+    def __init__(self, name, pay, phone):
+        super().__init__(name, job="Manager", pay=33333)
+        self.phone = phone
     def giveRaise(self):
         print("Giving Raise to Engineer")
+    def __str__(self):
+        return super().__str__() + " " + str(self.phone)
 
 if __name__ == "__main__":
     e = Person("Bob", "Assistant", 10000)
     f = Person("Jack")
     g = Person("Greg", job="Developer", pay=30000)
-    m = Manager("Smith", 20000)
+    m = Manager("Smith", 20000, 123456789)
     print(e)
     print(f)
     print(g)
     print(m)
+    print(m.phone)
+    print()
     for person in Person.all_persons:
         print(person)
 
